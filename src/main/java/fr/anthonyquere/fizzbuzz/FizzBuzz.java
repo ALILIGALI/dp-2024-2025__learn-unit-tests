79cac1ba5a87933227fdcc8b95146f4c10854314
ALILIGALI <mohamedaliligali@gmail.com>
2025-03-22 15:58:00 +0100

    voici le projet

--- src/main/java/fr/anthonyquere/fizzbuzz/FizzBuzz.java
+++ src/main/java/fr/anthonyquere/fizzbuzz/FizzBuzz.java
@@ -1,14 +1,34 @@
 package fr.anthonyquere.fizzbuzz;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FizzBuzz {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static List<String> startFizzBuzz(int count) {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 1; i <= count; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(String.valueOf(i));
+            }
+        }
+
+        return result;
     }
 
-    public static List<String> startFizzBuzz(int count) {
-        return null;
+    public static void main(String[] args) {
+        // Exécution avec un exemple
+        int count = 20; // Nombre d'éléments
+        List<String> output = startFizzBuzz(count);
+
+        // Affichage des résultats
+        output.forEach(System.out::println);
     }
 }
